29e6dcde55f31e411b2b8ea96b6db96ddaad6d9e
jorgeoj <jorge2002oj@gmail.com>
2024-03-20 18:09:11 +0100

    Añadidas las imagenes de todas las piezas y el talbero ya se pinta con las piezas incluidas (Aún no se pueden mover las piezas)

---
+++ Chess/ChessEngine.py
@@ -0,0 +1,24 @@
+"""
+Esta clase es responsable de almacenar toda la información sobre el estado actual de una partida de ajedrez. También
+será responsable de determinar los movimientos válidos en el estado actual y llevar un registro de los movimientos.
+"""
+class GameState():
+    def __init__(self):
+        # Tablero de 8x8 (lista 2D), cada elemento de la lista tiene 2 caracteres.
+        # El primer caracter representa el color de la pieza (b para negro y w para blanco), el segundo caracter
+        # representa el tipo de pieza (R para torre, N para caballo, B para alfil, Q para reina y K para rey)
+        # los caracteres "--" representan  un espacio vacío sin una pieza
+        self.board = [
+            ["bR", "bN", "bB", "bQ", "bK", "bB", "bN", "bR"],
+            ["bp", "bp", "bp", "bp", "bp", "bp", "bp", "bp"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["wp", "wp", "wp", "wp", "wp", "wp", "wp", "wp"],
+            ["wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"]
+        ]
+        # Variable para saber a quien le toca jugar (True si le toca a las blancas, False si le toca a las negras)
+        self.whiteToMove = True
+        # Lista para mantener un registro de los movimientos realizados durante la partida
+        self.moveLog = []
---
+++ Chess/ChessMain.py
@@ -0,0 +1,79 @@
+"""
+Este es nuestro archivo principal. Será responsable de manejar la entrada del usuario y mostrar el objeto gameState actual.
+"""
+
+import pygame as p # Importa la biblioteca pygame
+from Chess import ChessEngine # Importa el módulo ChessEngine desde el paquete Chess
+
+WIDTH = HEIGHT = 512 # Define el ancho y alto de la ventana del juego
+DIMENSION = 8 # Define las dimensiones del tablero de ajedrez (8x8)
+SQ_SIZE = HEIGHT // DIMENSION # Calcula el tamaño de cada cuadrado del tablero
+MAX_FPS = 15 # Establece el máximo de fps para animaciones más adelante
+IMAGES = {} # Diccionario global para almacenar las imágenes de las piezas del ajedrez
+
+'''
+Inicializa un diccionario global de imágenes. Esto se llamará exactamente una vez en el main.
+'''
+def loadImages():
+    pieces = ['wp', 'wR', 'wN', 'wB', 'wK', 'wQ', 'bp', 'bR', 'bN', 'bB', 'bK', 'bQ']
+    for piece in pieces:
+        # Carga las imágenes de las piezas y las escala al tamaño del cuadrado del tablero
+        IMAGES[piece] = p.transform.scale(p.image.load("images/" + piece + ".png"), (SQ_SIZE, SQ_SIZE))
+    # OJO:Podemos acceder a imagenes también poniendo: IMAGES['wp'] (wp como ejemplo de cada pieza)
+
+'''
+El main de nuestro código. Se encargará de la entrada del usuario y de actualizar los gráficos.
+'''
+def main():
+    p.init() # Inicializa Pygame
+    screen = p.display.set_mode((WIDTH, HEIGHT)) # Crea la ventana del juego
+    clock = p.time.Clock() # Objeto para controlar el tiempo del juego
+    screen.fill(p.Color("white"))
+    gs = ChessEngine.GameState() # Crea un objeto GameState para representar el estado del juego
+    loadImages() # Carga las imágenes de las piezas
+    running = True
+    while running:
+        for e in p.event.get():
+            # Si el usuario cierra la ventana, detiene el bucle principal
+            if e.type == p.QUIT :
+                running = False
+        drawGameState(screen, gs) # Dibuja el estado actual del juego en la pantalla
+        clock.tick(MAX_FPS) # Controla la velocidad de actualización de la pantalla
+        p.display.flip() # Actualiza la pantalla
+
+'''
+Responsable de todos los gráficos dentro del estado actual del juego.
+'''
+def drawGameState(screen, gs):
+    drawBoard(screen) # Esto dibuja los cuadrados en el tablero
+    drawPieces(screen, gs.board) # Esto dibuja las piezas encima de los cuadrados del tablero
+
+'''
+Dibujar los cuadrados en el tablero. OJO: El cuadrado de arriba a la izquierda del tablero siempre es blanco
+'''
+def drawBoard(screen):
+    colors = [p.Color("white"), p.Color("gray")] # Colores del tablero (se pueden cambiar mas adelante)
+    # El primer for recorre filas, el segundo recorre columnas. Dibuja cada cuadrado del tablero
+    for r in range(DIMENSION):
+        for c in range(DIMENSION):
+            color = colors[((r+c) % 2)] # Alterna los colores de los cuadrados para simular un tablero de ajedrez
+            p.draw.rect(screen, color, p.Rect(c*SQ_SIZE, r*SQ_SIZE, SQ_SIZE, SQ_SIZE))
+
+
+'''
+Dibujar las piezas en el tablero usando el GameState.board actual
+'''
+def drawPieces(screen, board):
+    for r in range(DIMENSION):
+        for c in range(DIMENSION):
+            piece = board[r][c] # Obtiene la pieza en la posición (r, c) del tablero
+            if piece != "--": # Nos aseguramos que no sea un cuadrado vacio, para dibujar la pieza
+                screen.blit(IMAGES[piece], p.Rect(c*SQ_SIZE, r*SQ_SIZE, SQ_SIZE, SQ_SIZE))
+
+
+if __name__ == "__main__":
+    main() # Ejecuta la función main si este archivo es el programa principal
+
+
+
+

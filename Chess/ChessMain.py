687f2ee1c45de38cacb95825ff09744ee7533acf
jorgeoj <jorge2002oj@gmail.com>
2024-04-23 19:23:54 +0200

    Pequeño cambio, tras terminar partida y pulsar R poder hacer movimientos (no se podia antes)

--- Chess/ChessMain.py
+++ Chess/ChessMain.py
@@ -86,6 +86,7 @@ def main():
                     playerClicks = []
                     moveMade = False
                     animate = False
+                    gameOver = False
 
         if moveMade:
             if animate:
